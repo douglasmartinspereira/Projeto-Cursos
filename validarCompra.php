12db593e474aeae8ddf151c3d2642c3419a5555e
unknown <Digital@DH-A2-29.dhbrasil.digitalhouse.com>
2019-04-06 12:35:04 -0300

    Aula 05 Funcoes

--- validarCompra.php
+++ validarCompra.php
@@ -6,8 +6,58 @@
     $cvv = $_REQUEST["cvv"];
     $nomeCurso = $_REQUEST["nomeCurso"];
     $precoCurso = $_REQUEST["precoCurso"];
+    $erros = [];
+
+    // criando funcoes para o formulario
+    function validarNome($nome){
+        return strlen($nome) > 0 && strlen($nome) <= 15;
+    }
+
+    function validarCpf($cpf){
+        return strlen($cpf) == 11;
+
+    }
+
+    function validarNroCartao($nroCartao){
+        $primeiroNum = substr($nroCartao, 0, 1);
+        return $primeiroNum == 4 || $primeiroNum == 5 || $primeiroNum == 6;
+    }
+
+    function validarData($data){
+        $dataAtual = date("Y-m");
+        return $data >= $dataAtual;
+    }
+
+    function validarCvv($cvv) {
+        return strlen($cvv) == 3;
+    }
+
+    function validarCompra($nome, $cpf, $nroCartao, $data, $cvv) {
+        global $erros;
+
+        if (!validarNome($nome)) {
+            array_push($erros, "Preencha seu nome");
+        }
+        if (!validarCpf($cpf)){
+            array_push($erros, "Seu Cpf precisa ter 11 caracteres");
+        }
+
+        if (!validarNroCartao($nroCartao)){
+            array_push($erros, "Seu Cartao precisa começar com 4, 5 ou 6");
+        }
+
+        if (!validarData($data)){
+            array_push($erros, "A validade precisa ser maior que a data atual");
+        }
+
+        if (!validarCvv($cvv)){
+            array_push($erros, "Seu Cvv precisa ter 3 caracteres");
 
 
+        }
+    }
+
+    validarCompra($nome, $cpf, $nroCartao, $validade, $cvv);
 
 ?>
 
@@ -25,6 +75,25 @@
 <body>
     <div class="container">
         <div class="col-md-6 col-md-offset-3">
+            <?php if (count($erros) > 0) : ?>
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <span>Preencha seus dados corretamente!</span>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-group">
+                        <?php foreach($erros as $chave => $valorErro) : ?>
+                            <li class="list-group-item">
+                                <?= $valorErro; ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <div class="center">
+                        <a href="index.php">Voltar para home</a>
+                    </div>
+                </div>
+            </div>
+            <?php else : ?>
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <span>Compra realizada com Sucesso!</span>
@@ -40,6 +109,7 @@
                     </div>
                 </div>
             </div>
+            <?php endif; ?>
         </div>
     </div>
     
